ccccaa358fc75fe6489d8e5f1486585fa3870243
Trevor DePew <t.depewmusic@gmail.com>
2023-04-07 07:15:39 -0400

    started porting portfolio site react over

---
+++ src/LandingPage.jsx
@@ -0,0 +1,63 @@
+import UnderConstruction from "./UnderConstruction"
+import SectionConstruction from "./SectionConstruction"
+import './App.css'
+import '../styling/landingPage.css'
+
+const LandingPage = () => {
+
+  return (
+    <div id="window">
+      <div id="home" className="content-container">
+        <div id="title-name">
+          <h1>Trevor DePew</h1>
+          <h4>Software Engineer</h4>
+        </div>
+        <div id="links">
+          <a id="about-link" href="#about">About</a>
+          <p>|</p>
+          <a id="experience-link" href="#experience">Experience</a>
+          <p>|</p>
+          <a id="projects-link" href="#projects">Projects</a>
+          <p>|</p>
+          <a id="contact-link" href="#contact">Contact</a>
+        </div>
+      </div>
+      <hr className="solid"></hr>
+      <div id="about" className="page-container">
+        <h2 className="section-title">About</h2>
+        <div className="content-container">
+          {/* <About /> */}
+          <SectionConstruction />
+        </div>
+      </div>
+      <hr className="solid"></hr>
+      {/* <div id="experience" className="page-container">
+        <h2 className="section-title">Experience</h2>
+        <div className="content-container"> */}
+          {/* <Experience /> */}
+        {/* </div> */}
+      {/* </div>
+      <hr className="solid"></hr>
+      <div id="projects" className="page-container">
+        <h2 className="section-title">Projects</h2>
+        <div className="content-container"> */}
+          {/* <Projects /> */}
+        {/* </div> */}
+      {/* </div>
+      <hr className="solid"></hr>
+      <div id="contact" className="page-container">
+        <h2 className="section-title">Contact</h2> */}
+        {/* <Contact /> */}
+      {/* </div> */}
+      {/* <hr className="solid"></hr>
+      <div id="footer">
+        <div className="contentContainer"> */}
+          {/* <Footer /> */}
+        {/* </div> */}
+      {/* </div> */}
+
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/SectionConstruction.jsx
@@ -0,0 +1,32 @@
+import '../styling/construction.css'
+
+const SectionConstruction = () => {
+  return (
+    <body>
+      <div className="graphic">
+        <div className="lights">
+          <div id="blinky-1" className="blinky-light"></div>
+          <div id="blinky-2" className="blinky-light"></div>
+          <div id="blinky-3" className="blinky-light"></div>
+        </div>
+        <div className="construction-bar"></div>
+      </div>
+      <div className="under-construction">
+        <span>Welcome to my website! </span>
+        <span>It's currently under construction.</span>
+      </div>
+      <div className="link-container">
+        <h5>In the meantime, check me out at these places:</h5>
+        <div className="links">
+          <a target="_blank" href="https://www.linkedin.com/in/trevor-depew/">LinkedIn</a>
+          <a target="_blank" href="https://github.com/tdep">GitHub</a>
+          <a target="_blank" href="./trevor_depew_resume.pdf">Resume</a>
+        </div>
+      </div>
+      <div id="root"></div>
+      <script type="module" src="/src/main.jsx"></script>
+    </body>
+  )
+}
+
+export default SectionConstruction
