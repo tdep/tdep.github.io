3eef86af9fcc222a8b3df64fad72ed493e904509
Trevor DePew <t.depewmusic@gmail.com>
2023-04-13 11:11:11 -0400

    moved svgs

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
     <App />
+    <img src="/public/trevor.svg" />
+    <img src="./public/trevor.svg" />
+    <img src="./public/portfolio-name-trevor.png" />  
+    <img src="./src/assets/portfolio-title-software.png" /> 
+    </>
 )
